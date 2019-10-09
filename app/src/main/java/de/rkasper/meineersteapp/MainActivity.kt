b95249d15ecab2546ea84bdc9af5f5ed84800d2c
mission-webstyle <kontakt@mission-webstyle.de>
2019-10-09 18:18:52 +0200

    Listener im XML definiert und in der MainActivity ausgewertet

--- app/src/main/java/de/rkasper/meineersteapp/MainActivity.kt
+++ app/src/main/java/de/rkasper/meineersteapp/MainActivity.kt
@@ -2,6 +2,9 @@ package de.rkasper.meineersteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.main_activity_layout.*
 
 /**
  * Einstiegspunkt in die App
@@ -14,6 +17,27 @@ class MainActivity : AppCompatActivity() {
 
         //1. Setzen des Layouts
         setContentView(R.layout.main_activity_layout)
+    }
+    //endregion
+
+
+    //region Listenerhandling per XML-Definiert
+    fun handleButtonClicksDefinedInLayout(v: View) {
+        when(v.id){
+            R.id.imgBtnShowFirstMonster -> showUserMsg("Number One")
+            R.id.imgBtnShowSecondMonster -> showUserMsg("Second")
+            R.id.imgBtnShowThirdMonster -> showUserMsg("Third in the row")
+            R.id.imgBtnShowFourthMonster -> showUserMsg("Four")
+
+        }
+    }
+    //endregion
+
+    //region UserMessage
+    fun showUserMsg(strMonsterTitle : String){
+        //UserMessage aus res/values/strings.xml auslesen und mit Titel verbind
+        val strUserMsg = getString(R.string.strUserMsgAMonsterChoosen) + strMonsterTitle;
+        Toast.makeText(this,strUserMsg,Toast.LENGTH_LONG).show()
     }
     //endregion
 }
