a73fcb0cd1b014cdad2aa0856ddf5547f0b4887f
booth14-mgr2 <booth14-mgr2@content.linkedin.biz>
2019-10-09 14:05:52 +0200

    Initial commit

---
+++ app/src/main/java/de/rkasper/meineersteapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package de.rkasper.meineersteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Einstiegspunkt in die App
+ */
+class MainActivity : AppCompatActivity() {
+
+    //region 1 Lebenszyklus
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //1. Setzen des Layouts
+        setContentView(R.layout.activity_main)
+    }
+    //endregion
+}
