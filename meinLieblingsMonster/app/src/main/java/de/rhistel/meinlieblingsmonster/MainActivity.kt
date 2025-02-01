543966eb8efafe7ad0fe2af60b8c5b4d527fe81b
Steffen Buchholz <steffen.buchholz@gmx.de>
2025-02-01 06:19:39 +0100

    Rebase on top of working version

--- meinLieblingsMonster/app/src/main/java/de/rhistel/meinlieblingsmonster/MainActivity.kt
+++ meinLieblingsMonster/app/src/main/java/de/rhistel/meinlieblingsmonster/MainActivity.kt
@@ -5,13 +5,22 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+<<<<<<< HEAD
 import android.view.View
+=======
+
+import kotlinx.android.synthetic.main.main_activity_layout.*
+>>>>>>> 6a64e85 (Initial Commit with all the files this time)
 
 /**
  * Einstiegspunkt in die App
  */
 class MainActivity : AppCompatActivity() {
+<<<<<<< HEAD
     private lateinit var binding: Binding
+=======
+
+>>>>>>> 6a64e85 (Initial Commit with all the files this time)
     //region 1 Lebenszyklus
 
     /**
@@ -21,6 +30,7 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< HEAD
         //1. Layout setzen
         this.setContentView(viewModel.layout.main_activity_layout)
 
@@ -33,17 +43,34 @@ class MainActivity : AppCompatActivity() {
 
         //3. Fab Button mit Listener belegen
         binding.fabSaveUserName.setOnClickListener { view: View ->
+=======
+
+        //1. Layout setzen
+        this.setContentView(R.layout.main_activity_layout)
+
+        //2. Setzen der Toolbar
+        this.setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+>>>>>>> 6a64e85 (Initial Commit with all the files this time)
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 6a64e85 (Initial Commit with all the files this time)
     //endregion
 
     //region 2. MenuHandling
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
+<<<<<<< HEAD
         menuInflater.inflate(binding.menu.menu_main, menu)
+=======
+        menuInflater.inflate(R.menu.menu_main, menu)
+>>>>>>> 6a64e85 (Initial Commit with all the files this time)
         return true
     }
 
@@ -52,9 +79,17 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
+<<<<<<< HEAD
             binding.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
 //endregion
+=======
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    //endregion
+>>>>>>> 6a64e85 (Initial Commit with all the files this time)
 }
